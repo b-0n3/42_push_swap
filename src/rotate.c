b0d5ffd52a94430b49161a8a5f0f9d6095489c34
lalouabd <lalouabd123@gmail.com>
2021-06-28 13:01:32 +0100

    feat(rotate)

---
+++ src/rotate.c
@@ -0,0 +1,57 @@
+#include "push_swap.h"
+
+// 1,2,3,4,5,6
+void	rotate_a(t_stacks *this, t_bool print)
+{
+    int i;
+    int *tmp;
+
+    if (this != NULL)
+    {
+        i = this->stack_a.index - 1;
+        tmp = this->stack_a.get(&this->stack_a, i);
+        if (print)
+            ft_putstr("ra\n");
+        while (i > 0)
+        {
+            this->stack_a.update_at(&this->stack_a,
+                 this->stack_a.get(&this->stack_a, i -1),
+                  i);
+            i--;
+        }
+        this->stack_a.update_at(&this->stack_a, tmp, 0);
+    }
+}
+
+void	rotate_b(t_stacks *this, t_bool print)
+{
+    int i;
+    int *tmp;
+
+    if (this != NULL)
+    {
+        i = this->stack_b.index - 1;
+        tmp = this->stack_b.get(&this->stack_b, i);
+        if (print)
+            ft_putstr("ra\n");
+        while (i > 0)
+        {
+            this->stack_b.update_at(&this->stack_b,
+                 this->stack_b.get(&this->stack_b, i -1),
+                  i);
+            i--;
+        }
+        this->stack_b.update_at(&this->stack_b, tmp, 0);
+    }
+}
+
+void	rotate_a_and_b(t_stacks *this)
+{
+    if (this != NULL)
+    {
+        rotate_a(this, FALSE);
+        rotate_b(this, FALSE);
+        ft_putstr("rrr\n");
+    }
+
+}
